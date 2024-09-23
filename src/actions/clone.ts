57dd6525f20d66e3197cf9137b068355325f06b5
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-09-23 15:47:05 -0400

    chore: update dig sdk

--- src/actions/clone.ts
+++ src/actions/clone.ts
@@ -1,11 +1,5 @@
-import fs from "fs";
-import path from "path";
-
 import {
   DigNetwork,
-  STORE_PATH,
-  DIG_FOLDER_PATH,
-  waitForPromise,
   DataStore,
 } from "@dignetwork/dig-sdk";
 
@@ -15,14 +9,6 @@ export const clone = async (
 ): Promise<void> => {
   try {
     console.log(`Cloning store: ${storeId}`);
-
-    const storeDir = path.join(DIG_FOLDER_PATH, "stores", storeId);
-
-    // Check if the store directory already exists
-    if (fs.existsSync(storeDir)) {
-      console.error(`Store with ID ${storeId} already exists at ${storeDir}.`);
-      process.exit(1); // Exit the process with an error code
-    }
 
     try {
       // Create an instance of DigNetwork
