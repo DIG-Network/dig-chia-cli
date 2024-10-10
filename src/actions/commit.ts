32c4d59d730c231f5c18fc15790f8b62fcfdb8c3
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-10-10 14:46:03 -0400

    chore: update sdk

--- src/actions/commit.ts
+++ src/actions/commit.ts
@@ -1,7 +1,8 @@
 import path from "path";
 import fs from "fs";
 import {
-  addDirectory,
+  listFilesRecursively,
+  asyncPool,
   calculateFolderSize,
   waitForPromise,
   DataStore,
@@ -29,34 +30,51 @@ export const commit = async (): Promise<void> => {
 
     const digConfig = await loadDigConfig(process.cwd());
 
-    await addDirectory(
-      dataStore.Tree,
-      path.join(process.cwd(), digConfig.deploy_dir)
+    // Group into 1000 files per batch to make management and upload easier
+    const fileGroups = listFilesRecursively(
+      path.join(process.cwd(), digConfig.deploy_dir),
+      1000
     );
 
-    const newRootHash = dataStore.Tree.commit();
+    const concurrencyLimit = 10;
 
-    if (!newRootHash) {
-      return;
+    for (const files of fileGroups) {
+      await asyncPool(concurrencyLimit, files as string[], async (file) => {
+        const filePath = path.join(process.cwd(), digConfig.deploy_dir, file);
+        const relativePath = file.replace(/\\/g, "/");
+        const stream = fs.createReadStream(filePath);
+        await dataStore.Tree.upsertKey(
+          stream,
+          Buffer.from(relativePath).toString("hex")
+        );
+      });
+
+      const newRootHash = dataStore.Tree.commit();
+
+      if (!newRootHash) {
+        return;
+      }
+
+      const totalBytes = calculateFolderSize(
+        path.resolve(STORE_PATH, dataStore.StoreId)
+      );
+
+      console.log(
+        `Updating store metadata with new root hash: ${newRootHash}, bytes: ${totalBytes}`
+      );
+
+      const updatedStoreInfo = await dataStore.updateMetadata({
+        ...latestStore.metadata,
+        rootHash: Buffer.from(newRootHash, "hex"),
+        bytes: totalBytes,
+      });
+
+      await FullNodePeer.waitForConfirmation(
+        updatedStoreInfo.coin.parentCoinInfo
+      );
+
+      await dataStore.fetchCoinInfo();
     }
-
-    const totalBytes = calculateFolderSize(
-      path.resolve(STORE_PATH, dataStore.StoreId)
-    );
-
-    console.log(
-      `Updating store metadata with new root hash: ${newRootHash}, bytes: ${totalBytes}`
-    );
-
-    const updatedStoreInfo = await dataStore.updateMetadata({
-      ...latestStore.metadata,
-      rootHash: Buffer.from(newRootHash, "hex"),
-      bytes: totalBytes,
-    });
-
-    await FullNodePeer.waitForConfirmation(
-      updatedStoreInfo.coin.parentCoinInfo
-    );
 
     await waitForPromise(
       () => dataStore.fetchCoinInfo(),
